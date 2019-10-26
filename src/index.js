1b8ab989daa9cb746069d397821456e8f4917101
Vladimir Ovechkin <ovchingus@gmail.com>
2019-10-26 15:31:07 -0700

    Finish i18n setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
 import MobileDetect from 'mobile-detect'
+
+import App from './App'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 import store, { actions as storeActions } from './store'
+// import i18n (needs to be bundled ;))
+import './i18n'
 
 const md = new MobileDetect(window.navigator.userAgent)
 const isMobile = Boolean(md.mobile())
